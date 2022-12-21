206a447a5303f88dd88c465652fab3836cebe79d
Ashley Shelton <63636009+jjtlemall@users.noreply.github.com>
2022-12-21 14:06:05 -0600

    All problems resolved

--- src/test/java/shelton/SheltonRestAssuredProjectTest.java
+++ src/test/java/shelton/SheltonRestAssuredProjectTest.java
@@ -15,17 +15,17 @@ public class SheltonRestAssuredProjectTest {
     String lastId ="";
 
     @Test
-    void testGetAll() {
-        Response response = get("http://localhost:" + port + "/api/reviews");
-        System.out.println(response.asString());
+    public void testGetAll() {
+        Response expect = get("https://api.open-meteo.com/v1/forecast?latitude=38.6270&longitude=90.1994&hourly=temperature_2m,relativehumidity_2m,windspeed_10m");
+        System.out.println(expect.asString());
 
-        int statusCode = response.getStatusCode();
+        int statusCode = expect.getStatusCode();
 
         Assert.assertEquals(statusCode, 204);
         System.out.println("Get all passed");
     }
     @Test
-    void testPost () {
+    public void testPost () {
         String requestBody = "{\n" +
                 "  \"stars\": 3, \n" +
                 "  \"productDescription\": \"very very good\", \n" +
@@ -33,24 +33,24 @@ public class SheltonRestAssuredProjectTest {
                 "  \"contactPhone\": \"12345678\", \n" +
                 "  \"contactEmail\": \"email@email.com\", \n" +
                 "  \"actionNeeded\": \"false\", \n}";
-        Response response = 
+        Response expect = 
                 given() 
                         .header("Content-type", "application/json")
                         .and()
                         .body(requestBody)
                         .when()
-                        .post("http://localhost:" + port + "/api/reviews/")
+                        .post("https://api.open-meteo.com/v1/forecast?latitude=38.6270&longitude=90.1994&hourly=temperature_2m,relativehumidity_2m,windspeed_10m")
                         .then()
                         .extract().response();
-        Assert.assertEquals(response.statusCode(), 201);
-        lastId = response.jsonPath().getString("id");
+        Assert.assertEquals(expect.statusCode(), 201);
+        lastId = expect.jsonPath().getString("id");
 
             System.out.println("last ID after post: " + lastId);
             System.out.println("Post passed");
     }
 
     @Test
-    void testPut () {
+    public void testPut () {
 
         String requestBody = "{\n" +
                 "  \"stars\": 3, \n" +
@@ -59,32 +59,32 @@ public class SheltonRestAssuredProjectTest {
                 "  \"contactPhone\": \"12345678\", \n" +
                 "  \"contactEmail\": \"email@email.com\", \n" +
                 "  \"actionNeeded\": \"true\", \n}";
-        Response response = 
+        Response expect = 
                 given()
                         .header("Content-type", "application/json")
                         .and()
                         .body(requestBody)
                         .when()
-                        .put("http://localhost:" + port + "/api/reviews/" + lastId)
+                        .put("https://api.open-meteo.com/v1/forecast?latitude=38.6270&longitude=90.1994&hourly=temperature_2m,relativehumidity_2m,windspeed_10m" + lastId)
                         .then()
                         .extract().response();
-            Assert.assertEquals(response.statusCode(), 200);
-            lastId= response.jsonPath().getString("id");
+            Assert.assertEquals(expect.statusCode(), 200);
+            lastId= expect.jsonPath().getString("id");
             System.out.println("last ID after put: " + lastId);
             System.out.println("Put passed");
 
     }
 
     @Test
-    void testDeleteById() {
-        Response response = 
+    public void testDeleteById() {
+        Response expect = 
             given() 
                     .header("Content-type", "application/json")
                     .when()
-                    .delete("http://localhost:" + port + "/api/reviews/" + lastId)
+                    .delete("https://api.open-meteo.com/v1/forecast?latitude=38.6270&longitude=90.1994&hourly=temperature_2m,relativehumidity_2m,windspeed_10m" + lastId)
                     .then()
                     .extract().response();
-        int statusCode = response.statusCode();
+        int statusCode = expect.statusCode();
 
         boolean validResult = (statusCode == 200) || (statusCode == 204);
         Assert.assertEquals(validResult, true);
