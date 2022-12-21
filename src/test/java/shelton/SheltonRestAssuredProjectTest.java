b5fe4586370f7bef553b9ade2aeabc193d160508
Ashley Shelton <63636009+jjtlemall@users.noreply.github.com>
2022-12-21 15:16:59 -0600

    is this working for you? line 90 is saying it doesnt like the 'Assert.assertEquals' bit and also my imports arent liking the annotation import and I cant figure out if i need to install something or what

--- src/test/java/shelton/SheltonRestAssuredProjectTest.java
+++ src/test/java/shelton/SheltonRestAssuredProjectTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import io.restassured.response.Response;
 import static io.restassured.RestAssured.*;
-import org.testng.annotations.*;
+import org.testng.annotations.Test;
 import org.testng.Assert;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
@@ -16,12 +16,12 @@ public class SheltonRestAssuredProjectTest {
 
     @Test
     public void testGetAll() {
-        Response expect = get("https://api.open-meteo.com/v1/forecast?latitude=38.6270&longitude=90.1994&hourly=temperature_2m,relativehumidity_2m,windspeed_10m");
-        System.out.println(expect.asString());
+        Response response = get("https://api.open-meteo.com/v1/forecast?latitude=38.6270&longitude=90.1994&hourly=temperature_2m,relativehumidity_2m,windspeed_10m");
+        System.out.println(response.asString());
 
-        int statusCode = expect.getStatusCode();
+        int statusCode = response.getStatusCode();
 
-        Assert.assertEquals(statusCode, 204);
+        Assert.assertEquals(statusCode, 200);
         System.out.println("Get all passed");
     }
     @Test
@@ -33,7 +33,7 @@ public class SheltonRestAssuredProjectTest {
                 "  \"contactPhone\": \"12345678\", \n" +
                 "  \"contactEmail\": \"email@email.com\", \n" +
                 "  \"actionNeeded\": \"false\", \n}";
-        Response expect = 
+        Response response = 
                 given() 
                         .header("Content-type", "application/json")
                         .and()
@@ -42,8 +42,8 @@ public class SheltonRestAssuredProjectTest {
                         .post("https://api.open-meteo.com/v1/forecast?latitude=38.6270&longitude=90.1994&hourly=temperature_2m,relativehumidity_2m,windspeed_10m")
                         .then()
                         .extract().response();
-        Assert.assertEquals(expect.statusCode(), 201);
-        lastId = expect.jsonPath().getString("id");
+        Assert.assertEquals(response.statusCode(), 204);
+        lastId = response.jsonPath().getString("id");
 
             System.out.println("last ID after post: " + lastId);
             System.out.println("Post passed");
@@ -59,17 +59,17 @@ public class SheltonRestAssuredProjectTest {
                 "  \"contactPhone\": \"12345678\", \n" +
                 "  \"contactEmail\": \"email@email.com\", \n" +
                 "  \"actionNeeded\": \"true\", \n}";
-        Response expect = 
+        Response response = 
                 given()
                         .header("Content-type", "application/json")
                         .and()
                         .body(requestBody)
                         .when()
-                        .put("https://api.open-meteo.com/v1/forecast?latitude=38.6270&longitude=90.1994&hourly=temperature_2m,relativehumidity_2m,windspeed_10m" + lastId)
+                        .put("https://api.open-meteo.com/v1/forecast?latitude=38.6270&longitude=90.1994&hourly=temperature_2m,relativehumidity_2m,windspeed_10m")
                         .then()
                         .extract().response();
-            Assert.assertEquals(expect.statusCode(), 200);
-            lastId= expect.jsonPath().getString("id");
+            Assert.assertEquals(response.statusCode(), 200);
+            lastId= response.jsonPath().getString("id");
             System.out.println("last ID after put: " + lastId);
             System.out.println("Put passed");
 
@@ -77,14 +77,14 @@ public class SheltonRestAssuredProjectTest {
 
     @Test
     public void testDeleteById() {
-        Response expect = 
+        Response response = 
             given() 
                     .header("Content-type", "application/json")
                     .when()
-                    .delete("https://api.open-meteo.com/v1/forecast?latitude=38.6270&longitude=90.1994&hourly=temperature_2m,relativehumidity_2m,windspeed_10m" + lastId)
+                    .delete("https://api.open-meteo.com/v1/forecast?latitude=38.6270&longitude=90.1994&hourly=temperature_2m,relativehumidity_2m,windspeed_10m")
                     .then()
                     .extract().response();
-        int statusCode = expect.statusCode();
+        int statusCode = response.statusCode();
 
         boolean validResult = (statusCode == 200) || (statusCode == 204);
         Assert.assertEquals(validResult, true);
